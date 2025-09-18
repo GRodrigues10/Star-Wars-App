09e1821dda233f44b80576b56d8e7bb501c08ffa
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-09-18 11:41:57 -0300

    feat(metadata):Implementar metadata do site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
 import "./globals.css";
 import StyleRegistry from "./lib/registry";
 
+
+export const metadata = {
+  title: "StarApp",
+  description: "Uma enciclopédia online de Star Wars de filmes, séries, documentários e personagens.",
+  icons: {
+    icon: '/star_.png',
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>
         <StyleRegistry>{children}</StyleRegistry>
       </body>
